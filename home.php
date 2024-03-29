6779bf3e928788f91cf493d55161fc9ee6a2fcac
kamalmagra <152066422+kamalmagra@users.noreply.github.com>
2024-03-29 14:20:57 +0530

    Update home.php

--- home.php
+++ home.php
@@ -13,7 +13,7 @@ if(isset($_SESSION['id']) && isset($_SESSION['user_name'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HOME</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="newstyle.css">
 </head>
 <body>
     <h1>hello, <?php echo $_SESSION['user_name']; ?></h1>
